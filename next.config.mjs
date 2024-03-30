391fc443ac893a78cdab71c2cbe5013e87c22075
anuradhedilshan <anuradhedilshan@gmail.com>
2024-03-30 07:14:13 +0530

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+const nextConfig = {
+  eslint: {
     ignoreDuringBuilds: true,
   }};
 
